66bf1f8a9d21c2cb0143767595a0923c0344db92
Akhmeett <mustapaahmet2007@gmail.com>
2026-03-25 22:52:15 +0500

    Delete Lab_7Web-Dev directory

--- Lab_7Web-Dev/task_1/informatics/1/a.py
+++
@@ -1,8 +0,0 @@
-import math
-
-a = int(input())
-b = int(input())
-
-hypotenuse = math.sqrt(a**2 + b**2)
-
-print(hypotenuse)
--- Lab_7Web-Dev/task_1/informatics/2/b.py
+++
@@ -1,8 +0,0 @@
-def power(a, n):
-    return a ** n
-
-data = input().split()
-a = float(data[0])
-n = int(data[1])
-
-print(power(a, n))
